745c598349dbc93ebfa3d8ebb3456ba643fbc91b
Linda Pedroza <lsandoe@elp.rr.com>
2013-11-03 20:45:37 -0700

    fixed problem with routingby adding .htaccess file

---
+++ controllers/c_practice.php
@@ -0,0 +1,15 @@
+<?php
+
+class practice_controller extends base_controller {
+	
+
+	public function test_db() {
+	$q = "some dumb message";
+	
+	echo $q;	
+			
+	}
+	
+} # eoc
+
+?>
--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,11 +1,22 @@
-<p>
-	Hello World! You have successfully spawned a new application.
-</p>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Welcome to Chit Chat!</title>
+</head>
+<body>
+<h1>Chit Chat!</h1>
+<p>Chit Chat is a quick and friendly way to either chit chat with your friends, or keep them posted on what you're doing throughout your day.</p>
 
-<p>
-	This message is being triggered via the c_index.php controller, within the index() method.
-</p>
+<form action = "v_signup_confirmed.php" method = "post">
+<h2>Please Fill in the following information below so we can get you Chit Chatting.</h2><br><br>
+       First Name:  <input type = "text" name = first name><br><br>
+        Last Name:   <input type = "text" name = last name> <br><br>
+    Email Address: <input type = "text" name = "email" /><br><br>
+         Password: <input type ="password" name = "password" /><br><br>
+Re enter Password: <input type ="password" name = "password" /><br><br>
+Small personal Bio: <input type = "Bio" name = "Bio"><br><br>
+<input type = "submit" value = "Sign Up"
+</form>
 
-<p>
-	<strong>Since everything is in working order, you should now delete <?php echo APP_PATH?>diagnostics.php</strong>
-</p>
+</body>
+</html>
