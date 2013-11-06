77c6fef00bf3d2c84ea6578666b8c31e1ed53dc6
Linda Pedroza <lsandoe@elp.rr.com>
2013-11-05 19:29:24 -0700

    working model

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -4,9 +4,30 @@ class practice_controller extends base_controller {
 	
 
 	public function test_db() {
-	$q = "some dumb message";
+		/*$q = "UPDATE users
+		SET email = 'albert@aol.com'
+		WHERE first_name = 'albert'"; 
+		
+		
+		
+		
+		
+		/*"INSERT INTO users SET 
+		first_name = 'Albert',
+		last_name = 'Einstein'";
+	    */
+		
+		
+		$new_user = Array(
+		first_name => 'albert',
+		last_name => 'einstein',
+		email => 'albert@gmail.com',);
+		
+		
+	echo $q; 
 	
-	echo $q;	
+		
+	DB::instance(DB_NAME)->insert('users', $new_user);	
 			
 	}
 	
--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,22 +1,12 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Welcome to Chit Chat!</title>
-</head>
-<body>
-<h1>Chit Chat!</h1>
-<p>Chit Chat is a quick and friendly way to either chit chat with your friends, or keep them posted on what you're doing throughout your day.</p>
-
-<form action = "v_signup_confirmed.php" method = "post">
-<h2>Please Fill in the following information below so we can get you Chit Chatting.</h2><br><br>
-       First Name:  <input type = "text" name = first name><br><br>
-        Last Name:   <input type = "text" name = last name> <br><br>
-    Email Address: <input type = "text" name = "email" /><br><br>
-         Password: <input type ="password" name = "password" /><br><br>
-Re enter Password: <input type ="password" name = "password" /><br><br>
-Small personal Bio: <input type = "Bio" name = "Bio"><br><br>
-<input type = "submit" value = "Sign Up"
-</form>
-
-</body>
-</html>
+<?php if($user): ?>
+ 
+ 		<pre>
+        	<?php
+        		print_r($user);
+			?>	
+        </pre>
+        Hello <?=$user->first_name;?>
+<?php else: ?>
+        Welcome Chit Chatter!<br>
+		This is a chatting blog to not only chat with others, but follow what they are up to as well.  So lets get started and become part of the chatting network!      
+<?php endif; ?>
